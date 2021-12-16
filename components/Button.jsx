5bb3b91848cd60e074d65d4e89fce26ad541491f
Asti7 <astitvagautam@gmail.com>
2021-12-16 12:20:27 +1100

    WIP: Home page layout

---
+++ components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({label})=>{
+
+    return <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-80">
+        {label}
+    </button>
+
+}
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
-import Head from 'next/head'
+
 import Image from 'next/image'
+import Button from '../components/Button'
 
 
 const HomePage = () => {
 
-  return <div>
+  return <div className='flex flex-col gap-60 justify-center items-center content-between h-screen'>
+
+    <p className='text-2xl'>PROJECT MULTI</p>
+
+    <div className='flex flex-col gap-4'>
+      <Button label="LOGIN" />
+      <Button label="SIGNUP" />
+    </div>
+
+
   </div>
 
 }
-
-
 
 export default HomePage
 
