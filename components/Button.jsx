9265e2a58984d372c2c42fc792cfc5a6fabc59cc
Asti7 <astitvagautam@gmail.com>
2021-12-16 13:00:57 +1100

    Added Form lib for managing forms and it's state

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,6 +1,7 @@
-const Button = ({label})=>{
+const Button = ({label,onClick})=>{
 
-    return <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-80">
+    return <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-80"
+    onClick={onClick}>
         {label}
     </button>
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-
-import Image from 'next/image'
-import Button from '../components/Button'
-
+import Router from 'next/router';
+import Button from '../components/Button';
 
 const HomePage = () => {
 
@@ -10,8 +8,8 @@ const HomePage = () => {
     <p className='text-2xl'>PROJECT MULTI</p>
 
     <div className='flex flex-col gap-4'>
-      <Button label="LOGIN" />
-      <Button label="SIGNUP" />
+      <Button label="LOGIN" onClick = {()=> Router.push('/login')}/>
+      <Button label="SIGNUP" onClick = {() => {}}/>
     </div>
 
 
