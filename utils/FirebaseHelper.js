9ea465b12cea3423a37fb10139183a7b5c8fd720
SuryaKannan <surya.kannan@icloud.com>
2022-04-29 03:30:06 +1000

    [feature] Added helper class for backend reads

--- utils/FirebaseHelper.js
+++ utils/FirebaseHelper.js
@@ -1,19 +1,77 @@
+/*
+Firebase helper class
+
+Usage: 
+
+1) instantiate new class 
+2) call in useEffect 
+3) retrieve required information to render elements via class properties
+
+
+e.g.
+import {useEffect} from "react";
+import {auth} from "../utils/firebase.config";
+import {FirebaseHelper} from "../utils/FirebaseHelper"
+
+const firebaseHelper = new FirebaseHelper();
+
+ const testFunction = () => { // called with onClick
+    console.log(firebaseHelper.teams)
+}
+
+const getUserDetails = () => {
+    auth.onAuthStateChanged((currentUser) => { 
+        firebaseHelper.id = currentUser.uid;
+        firebaseHelper.get_details();
+        firebaseHelper.get_teams();
+        firebaseHelper.get_mdms();
+    })
+}
+
+useEffect(() => {
+    getUserDetails();
+},[]);
+*/
+
 import {db} from "./firebase.config";
 
 export class FirebaseHelper { 
     constructor(){
         this.id;
         this.userRef;
+        this.first_name;
+        this.last_name;
+        this.email;
+        this.organisation; 
+        this.teams = []; 
+        this.mdms = [];
     }
 
-    gen_userRef(){
-        this.userRef = db.collection('users').doc(this.id);
+    get_userRef(){
+        if (!this.userRef){
+            this.userRef = db.collection('users').doc(this.id);
+        }
     }
 
-    get teams() {
+    get_details(){
+
+        this.get_userRef();
+        this.userRef.onSnapshot((doc) => { // attach listener to user doc
+            if (doc.exists) { 
+                this.first_name = doc.data().first_name;
+                this.last_name = doc.data().last_name;
+                this.email = doc.data().email;
+                this.organisation = doc.data().organisation;
+            } else {
+                console.log("No such document!");
+            }
+        });
+
+    }
+
+    get_teams() {
         
-        const teamList = []
-
+        this.get_userRef();
         this.userRef.onSnapshot((doc) => { // attach listener to user doc
             if (doc.exists) { 
                 if(doc.data().teams){
@@ -21,7 +79,7 @@ export class FirebaseHelper {
                     teams.forEach(async (teamRef)=> { 
                         const team = await teamRef.get();
                         if (team.exists){
-                            teamList.push(team.id,team.data())
+                            this.teams.push(team.id,team.data())
                         }
                     })
                 }
@@ -29,6 +87,25 @@ export class FirebaseHelper {
                 console.log("No such document!");
             }
         });
-        return teamList
+    }
+
+    get_mdms() {
+        
+        this.get_userRef();
+        this.userRef.onSnapshot((doc) => { // attach listener to user doc
+            if (doc.exists) { 
+                if(doc.data().mdms){
+                    const mdms = doc.data().mdms; // get list of references to mdms linked to user 
+                    mdms.forEach(async (mdmRef)=> { 
+                        const mdm = await mdmRef.get();
+                        if (mdm.exists){
+                            this.mdms.push(mdm.id,mdm.data())
+                        }
+                    })
+                }
+            } else {
+                console.log("No such document!");
+            }
+        });
     }
 }
